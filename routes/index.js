9884bfc43bcd738ec00e9097e890120317ccf8ec
Eli Hansen <elihansen@yahoo.com>
2026-05-23 14:23:53 -0600

    Temporarily disable uncompleted swagger route for rest testing

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/', require('./swagger'));
+// router.use('/', require('./swagger'));
 router.use('/recipes', require('./recipesRoute'));
 router.use('/ingredients', require('./ingredientsRoute'));
 
