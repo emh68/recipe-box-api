07b8ad338c189800c94378c2af7c0cd74688e668
Eli Hansen <elihansen@yahoo.com>
2026-05-23 16:32:55 -0600

    Testing can get to api-docs, but not /recipes or /ingredients anymore.

--- db/connect.js
+++ db/connect.js
@@ -6,28 +6,17 @@ const client = new MongoClient(process.env.MONGODB_URI);
 let _db;
 
 const initDb = (callback) => {
-    if (_db) {
-        console.log('Db is already initialized!');
-        return callback(null, _db);
-    }
-
-    console.log('Connecting to MongoDB...');
-    MongoClient.connect(process.env.MONGODB_URI)
+    if (_db) return callback(null, _db);
+    client.connect()
         .then((client) => {
-            _db = client.db('recipe-box');
-            console.log('Successfully connected to MongoDB.');
+            _db = client;
             callback(null, _db);
         })
-        .catch((err) => {
-            console.error('Database connection error:', err);
-            callback(err);
-        });
+        .catch((err) => callback(err));
 };
 
 const getDb = () => {
-    if (!_db) {
-        throw Error('Db not initialized');
-    }
+    if (!_db) throw Error('Db not initialized');
     return _db;
 };
 
