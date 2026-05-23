41773c99b0b3ad18dde6cd0af37411981c41aa99
Eli Hansen <elihansen@yahoo.com>
2026-05-23 16:24:20 -0600

    Testing errors and conneciton to db.

--- db/connect.js
+++ db/connect.js
@@ -3,15 +3,32 @@ require('dotenv').config();
 
 const client = new MongoClient(process.env.MONGODB_URI);
 
-let db;
+let _db;
 
 const initDb = (callback) => {
-    client.connect().then(() => {
-        db = client.db('recipe-box');
-        callback(null, db);
-    }).catch(err => callback(err));
+    if (_db) {
+        console.log('Db is already initialized!');
+        return callback(null, _db);
+    }
+
+    console.log('Connecting to MongoDB...');
+    MongoClient.connect(process.env.MONGODB_URI)
+        .then((client) => {
+            _db = client.db('recipe-box');
+            console.log('Successfully connected to MongoDB.');
+            callback(null, _db);
+        })
+        .catch((err) => {
+            console.error('Database connection error:', err);
+            callback(err);
+        });
 };
 
-const getDb = () => db;
+const getDb = () => {
+    if (!_db) {
+        throw Error('Db not initialized');
+    }
+    return _db;
+};
 
-module.exports = {initDb, getDb};
+module.exports = { initDb, getDb };
