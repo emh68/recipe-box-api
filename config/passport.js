396cd1c7894af7103d6310133ba45aae25fcd8f2
Eli Hansen <elihansen@yahoo.com>
2026-06-06 22:55:28 -0600

    Updated swagger docs and tags for google authentication.

--- config/passport.js
+++ config/passport.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:8080/auth/google/callback'
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
 },
     async (accessToken, refreshToken, profile, done) => {
         const userProfile = {
--- routes/auth.js
+++ routes/auth.js
@@ -3,16 +3,21 @@ const router = express.Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'], session: false }));
+router.get('/google', /* #swagger.tags = ['Authentication'] */ passport.authenticate('google', { scope: ['profile', 'email'], session: false }));
 
 
-router.get('/google/callback',
+router.get('/google/callback', /* #swagger.tags = ['Authentication'] */
     passport.authenticate('google', { failureRedirect: '/auth/failure', session: false }),
     (req, res) => {
         const token = jwt.sign(
-            { id: req.user.goggleId, email: req.user.email },
+            {
+                id: req.user.googleId,
+                email: req.user.email
+            },
             process.env.JWT_SECRET,
-            { expiresIn: '1h' }
+            {
+                expiresIn: '1h'
+            }
         );
         res.json({
             message: "Authentication successful.",
@@ -21,7 +26,7 @@ router.get('/google/callback',
     }
 );
 
-router.get('/failure', (req, res) => {
+router.get('/failure', /* #swagger.tags = ['Authentication'] */(req, res) => {
     res.status(401).json({ message: "Google authentication failed." });
 });
 
